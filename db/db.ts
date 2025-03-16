917a55f7f3562eef5b7531b857f5f12eb5aefcb7
Gil <ghopecs@gmail.com>
2025-03-15 22:44:14 -0400

    add queries

--- db/db.ts
+++ db/db.ts
@@ -1,10 +1,15 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { profilesTable } from "./schema";
+import { todosTable } from "./schema";
 
 config({ path: ".env.local" });
 
-const schema = {};
+const schema = {
+  profiles: profilesTable,
+  todos: todosTable
+};
 
 const client = postgres(process.env.DATABASE_URL!);
 
